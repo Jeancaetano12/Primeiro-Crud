5485d17a149608f76ed59e60346c93e6f9028eb8
Jean Caetano <caetanojean4@gmail.com>
2025-08-10 00:02:01 -0300

    ADS: <01735073> Next-js

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,17 @@
+/**
+ * Nome do arquivo: eslint.config.mjs
+ * Data de criação: 09/08/2025
+ * Autor: Jean Caetano
+ * Matrícula: 01735073
+ *
+ * Descrição:
+ * Este arquivo JavaScript é responsável por implementar as funcionalidades
+ * de interação do usuário com a interface gráfica do módulo de vendas.
+ * Aqui são tratados eventos de cliques, validações de entrada e comunicação
+ * com APIs para envio e recebimento de dados.
+ *
+ * Este script é parte o curso de ADS.
+ */
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
